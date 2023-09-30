48d11cc4ec3525220fc9824351fd41753f545dd2
Michael Dvin <idewishortcut@gmail.com>
2023-09-30 16:04:15 +0500

    Fix tests

--- src/GitActionsProject/Assets/CodeBase/Tests/HealthTests.cs
+++ src/GitActionsProject/Assets/CodeBase/Tests/HealthTests.cs
@@ -24,8 +24,7 @@ namespace CodeBase.Tests
             health.TakeDamage(damage);
             
             // Assert.
-            // Broken
-            health.Value.Should().Be(initialHealthValue + damage);
+            health.Value.Should().Be(initialHealthValue - damage);
         }
     }
 }
